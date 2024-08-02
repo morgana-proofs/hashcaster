404e3b8451804c4bb5e3f686d4bb9b8f07e1de78
Lev <0xdeadfae@gmail.com>
2024-08-02 08:32:18 +0400

    started lincheck, currently not done

--- src/protocols/lincheck.rs
+++ src/protocols/lincheck.rs
@@ -1,6 +1,8 @@
-use num_traits::Zero;
+use num_traits::{One, Zero};
 
-use crate::field::F128;
+use crate::{field::F128, utils::log2_exact};
+
+use super::prodcheck::Prodcheck;
 
 pub trait LinOp {
     fn n_in(&self) -> usize;
@@ -25,7 +27,6 @@ impl<A: LinOp, B: LinOp> Composition<A, B> {
 }
 
 impl<A: LinOp, B: LinOp> LinOp for Composition<A, B> {
-        
     fn n_in(&self) -> usize {
         self.b.n_in()
     }
@@ -47,5 +48,49 @@ impl<A: LinOp, B: LinOp> LinOp for Composition<A, B> {
         self.a.apply_transposed(input, &mut tmp);
         self.b.apply_transposed(&tmp, output);
     }
+}
 
+
+/// Represents a linear sumcheck of the form
+/// M(pt_0, ... pt_{a-1}; x_0, ..., x_{a-1}) * P(x_0, ..., x_{a-1}, pt_a, ..., pt_{n-1}),
+/// where `a` is a number of "active" variables.
+/// It is very small, and the main computational work is computing the restriction
+/// P(x_0, ..., x_{a-1}, pt_a, ..., pt_{n-1}).
+/// M(pt_0, ... pt_{a-1}; x_0, ..., x_{a-1}) is computed by applying the transposition of matrix M
+/// to the vector of values of a polynomial eq_poly(pt[0..a]).
+/// Lincheck expects a matrix of size N*2^a x M*2^a, and it will be treated as matrix from N chunks of
+/// size 2^a to M chunks of size 2^a.
+pub struct Lincheck<const N: usize, const M: usize, L: LinOp> {
+    matrix: L,
+    polys: [Vec<F128>; N],
+    pt: Vec<F128>,
+    num_vars: usize,
+    num_active_vars: usize,
+}
+
+impl<const N: usize, const M: usize, L: LinOp> Lincheck<N, M, L> {
+    pub fn new(polys: [Vec<F128>; N], pt: Vec<F128>, matrix: L, num_active_vars: usize) -> Self {
+        assert!(matrix.n_in() == N * (1 << num_active_vars));
+        assert!(matrix.n_out() == M * (1 << num_active_vars));
+        let num_vars = pt.len();
+        assert!(num_vars >= num_active_vars);
+        for i in 0..N {
+            assert!(polys[i].len() == 1 << num_vars);
+        }
+        Self { matrix, polys, pt, num_vars, num_active_vars }
+    } 
+
+    pub fn folding_challenge(self, gamma: F128) -> PreparedLincheck {
+        let mut gamma_pows = Vec::with_capacity(M);
+        let mut tmp = F128::one();
+        for _ in 0..M {
+            gamma_pows.push(tmp);
+            tmp *= gamma;
+        }
+        let eq = eq_poly();
+    }
+}
+
+pub struct PreparedLincheck {
+    object: Prodcheck
 }
