a6dcdbef569f6c0dbcf2df87df21c74afca79791
Lev Soukhanov <->
2026-07-01 04:59:55 +0300

    Flatten restrict parallel work

--- src/protocols/utils.rs
+++ src/protocols/utils.rs
@@ -315,54 +315,63 @@ pub fn restrict(polys: &[&[F128]], coords: &[F128], dims: usize, eq: &mut [F128]
     const TILE: usize = 8;
     let num_tiles = (num_chunks + TILE - 1) / TILE;
 
-    for q in 0..n {
-        #[cfg(not(feature = "parallel"))]
-        let iter = (0..num_tiles).into_iter();
-        #[cfg(feature = "parallel")]
-        let iter = (0..num_tiles).into_par_iter().with_min_len(4);
-        iter.map(|tile| {
-            let start = tile * TILE;
-            let len = (num_chunks - start).min(TILE);
-            let mut acc = [[F128::zero(); 128]; TILE];
+    let process_tile = |q: usize, tile: usize| {
+        let start = tile * TILE;
+        let len = (num_chunks - start).min(TILE);
+        let mut acc = [[F128::zero(); 128]; TILE];
 
-            for b in 0..len {
-                let i = start + b;
-                for j in 0 .. eq.len() / 16 { // Step by 16
-                    let v0 = &eq_sums[j * 512 .. j * 512 + 256];
-                    let v1 = &eq_sums[j * 512 + 256 .. j * 512 + 512];
-                    let bytearr = cast_slice::<F128, [u8; 16]>(
-                        &polys[q][i * chunk_size + j * 16 .. i * chunk_size + (j + 1) * 16]
-                    );
+        for b in 0..len {
+            let i = start + b;
+            for j in 0 .. eq.len() / 16 { // Step by 16
+                let v0 = &eq_sums[j * 512 .. j * 512 + 256];
+                let v1 = &eq_sums[j * 512 + 256 .. j * 512 + 512];
+                let bytearr = cast_slice::<F128, [u8; 16]>(
+                    &polys[q][i * chunk_size + j * 16 .. i * chunk_size + (j + 1) * 16]
+                );
 
-                    // Iteration over bytes
-                    for s in 0..16 {
-                        let mut t = [
-                            bytearr[0][s], bytearr[1][s], bytearr[2][s], bytearr[3][s],
-                            bytearr[4][s], bytearr[5][s], bytearr[6][s], bytearr[7][s],
-                            bytearr[8][s], bytearr[9][s], bytearr[10][s], bytearr[11][s],
-                            bytearr[12][s], bytearr[13][s], bytearr[14][s], bytearr[15][s],
-                        ];
+                // Iteration over bytes
+                for s in 0..16 {
+                    let mut t = [
+                        bytearr[0][s], bytearr[1][s], bytearr[2][s], bytearr[3][s],
+                        bytearr[4][s], bytearr[5][s], bytearr[6][s], bytearr[7][s],
+                        bytearr[8][s], bytearr[9][s], bytearr[10][s], bytearr[11][s],
+                        bytearr[12][s], bytearr[13][s], bytearr[14][s], bytearr[15][s],
+                    ];
 
-                        for u in 0..8 {
-                            let bits = v_movemask_epi8(t) as u16;
-                            acc[b][s*8 + 7 - u] += v0[(bits & 255) as usize];
-                            acc[b][s*8 + 7 - u] += v1[((bits >> 8) & 255) as usize];
-                            t = v_slli_epi64::<1>(t);
-                        }
-                    }
-                }
-            }
-
-            unsafe {
-                for coord in 0..128 {
-                    let out = (coord + q * 128) * num_chunks + start;
-                    for b in 0..len {
-                        *ret_ptr.get_mut(out + b) = acc[b][coord];
+                    for u in 0..8 {
+                        let bits = v_movemask_epi8(t) as u16;
+                        acc[b][s*8 + 7 - u] += v0[(bits & 255) as usize];
+                        acc[b][s*8 + 7 - u] += v1[((bits >> 8) & 255) as usize];
+                        t = v_slli_epi64::<1>(t);
                     }
                 }
             }
         }
-        ).count();
+
+        unsafe {
+            for coord in 0..128 {
+                let out = (coord + q * 128) * num_chunks + start;
+                for b in 0..len {
+                    *ret_ptr.get_mut(out + b) = acc[b][coord];
+                }
+            }
+        }
+    };
+
+    #[cfg(not(feature = "parallel"))]
+    {
+        for q in 0..n {
+            for tile in 0..num_tiles {
+                process_tile(q, tile);
+            }
+        }
+    }
+
+    #[cfg(feature = "parallel")]
+    {
+        (0..n * num_tiles).into_par_iter().with_min_len(8).for_each(|work| {
+            process_tile(work / num_tiles, work % num_tiles);
+        });
     }
 }
 
